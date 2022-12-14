16c78296197731c39811c3d62b78f430ce39f147
Shubham Rajput <rajput.shubh1993@gmail.com>
2022-12-14 21:35:01 +0530

    All the ways of conditionally rendering:
    1. Simple if-else
    2. Using variable
    3. ternary operator
    4. Logical rendering

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -11,11 +11,34 @@ class UserGreeting extends Component{
   }
 
   render(){
+  /* simple if else
+
     if(this.state.isLoggedIn){
       return <div>Welcome Shubham</div>
     }else{
       return <div>Welcome Guest</div> 
     }
+  */
+
+  /* using variable 
+
+    let message
+    if(this.state.isLoggedIn){
+      message = <div>Welcome Shubham</div>       
+    }else{
+      message = <div>Welcome Guest</div> 
+    }
+
+    return <div>{message}</div>
+  */
+
+  /* ternary operator
+
+    return this.state.isLoggedIn ? <div>Welcome Shubham</div> : <div>Welcome Guest</div>
+  */
+
+  /* logical way */
+    return this.state.isLoggedIn && <div>Welcome Shubham</div>
   }
 }
 
