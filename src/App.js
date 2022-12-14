2921ff8754b7312d8444bf6f540922515c84da22
Shubham Rajput <rajput.shubh1993@gmail.com>
2022-12-14 21:12:24 +0530

    Conditional rendering using if else

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import ParentComponent from "./components/ParentComponent"
-
+import UserGreeting from "./components/UserGreeting"
 function App() {
   return (
     <div className="App">
-      <ParentComponent />
+      <UserGreeting />
     </div>
   )
 }
---
+++ src/components/UserGreeting.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+
+class UserGreeting extends Component{
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      isLoggedIn: false
+    }
+  }
+
+  render(){
+    if(this.state.isLoggedIn){
+      return <div>Welcome Shubham</div>
+    }else{
+      return <div>Welcome Guest</div> 
+    }
+  }
+}
+
+export default UserGreeting
