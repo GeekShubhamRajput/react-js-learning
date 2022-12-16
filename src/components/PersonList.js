68cdc8ee1ca2a46cf2bc7b0a224dadab0de4d86a
Shubham Rajput <rajput.shubh1993@gmail.com>
2022-12-16 19:20:54 +0530

    Lists and keys

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Person from './Person'
 
 function PersonList(){
-  var people = [ { name: "John", age: 21, skill: "React" }, { name: "Clark", age: 22, skill: "Vue" }, { name: "Mark", age: 23, skill: "Angular" }]
+  var people = [ { id: 1, name: "John", age: 21, skill: "React" }, { id: 2, name: "Clark", age: 22, skill: "Vue" }, { id: 3, name: "Mark", age: 23, skill: "Angular" }]
   
-  const personList = people.map(person => (<Person person={person}></Person>))
+  const personList = people.map(person => (<Person key={person.id} person={person}></Person>))
    
   return <div>{personList}</div>
 }
