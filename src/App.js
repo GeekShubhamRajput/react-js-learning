8757faff40b8fced05c052c0471dcddfccdc0de7
Shubham Rajput <rajput.shubh1993@gmail.com>
2022-12-20 11:00:24 +0530

    Add CSS externally

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import UserGreeting from "./components/UserGreeting"
 import PersonList from "./components/PersonList"
 import CarCompany from "./components/CarCompany"
+import "./myStyle.css"
+import StyleSheet from "./components/StyleSheet"
 
 function App() {
   return (
     <div className="App">
       { /* <UserGreeting /> */ }
-      <PersonList />
-      <CarCompany />
+      { /* <PersonList /> */ }
+      { /* <CarCompany /> */ }
+      <StyleSheet primary={false} />
     </div>
   )
 }
---
+++ src/components/StyleSheet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function StyleSheet(props){
+  let className = props.primary ? 'primary' : 'secondary' 
+  return <h2 className={className}> This is my Heading</h2>
+}
+
+export default StyleSheet
