f1e5b9d873a26e5fe2633e79dfc57fdc57b3ed44
Shubham Rajput <rajput.shubh1993@gmail.com>
2023-01-03 17:41:45 +0530

    Callback ref
    Focus on second input field using callback ref

--- src/components/RefsDemo.js
+++ src/components/RefsDemo.js
@@ -6,11 +6,19 @@ class RefsDemo extends Component{
     super(props);
 
     this.inputRef = React.createRef()
+    this.cbRef = 'null'
+
+    this.setCbRef = (element) => {
+      this.cbRef = element
+    }
   }
 
   componentDidMount(){
-    console.log(this.inputRef)
-    this.inputRef.current.focus();
+    if (this.cbRef) {
+      this.cbRef.focus() 
+    }
+    // console.log(this.inputRef)
+    // this.inputRef.current.focus();
   }
 
   clickHandler = () => {
@@ -21,6 +29,7 @@ class RefsDemo extends Component{
     return(
       <div>
         <input type="text" ref={this.inputRef} />
+        <input type="text" ref={this.setCbRef} />
         <button onClick={this.clickHandler}>click</button>
       </div>
     )
