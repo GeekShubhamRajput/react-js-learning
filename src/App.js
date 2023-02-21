6ef2ea989a60142769346bbfafce43fdd4aca53e
Shubham Rajput <rajput.shubh1993@gmail.com>
2023-02-21 12:40:55 +0530

    useReducer with useContext
    Implement count using useReducer and useContext both

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, {useReducer} from 'react'
 import './App.css';
-import CounterThree from "./components/CounterThree"
+import ComponentAA from "./components/ComponentAA"
+import ComponentBB from "./components/ComponentBB"
+import ComponentCC from "./components/ComponentCC"
+
+const initialState = 0;
+const reducer=(state, action)=>{
+  switch(action){
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state -1
+    case 'reset':
+      return initialState
+    default: 
+      return state
+  }
+}
+
+export const CountContext = React.createContext()
 
 function App() {
+  const [counter, dispatch] = useReducer(reducer, initialState) 
+
   return(
+    <CountContext.Provider value={{countState: counter, countDispatch: dispatch}}>
     <div className="App">
-      <CounterThree />
+      Count - {counter}  
+      <ComponentAA />
+      <ComponentBB />
+      <ComponentCC />
     </div>
+    </CountContext.Provider>
   )
 }
 
---
+++ src/components/ComponentDD.js
@@ -0,0 +1,17 @@
+import React, {useContext} from 'react'
+import {CountContext} from "../App"
+
+function ComponentDD(){
+  const countContext = useContext(CountContext)
+
+  return(
+    <div>
+      Component D : {countContext.countState}
+      <button onClick={()=>countContext.countDispatch('increment')}>Increment</button>
+      <button onClick={()=>countContext.countDispatch('reset')}>Reset</button>
+      <button onClick={()=>countContext.countDispatch('decrement')}>Decrement</button>
+    </div>
+  )
+}
+
+export default ComponentDD
