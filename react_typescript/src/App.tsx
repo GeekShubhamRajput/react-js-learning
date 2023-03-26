d3ddc80a51d8a63ae1ff7d8aa7d59ca15476b207
Shubham Rajput <rajput.shubh1993@gmail.com>
2023-03-26 16:27:59 +0530

    Advanced Props
    pass status props to component

--- react_typescript/src/App.tsx
+++ react_typescript/src/App.tsx
@@ -1,6 +1,7 @@
 import {Greet} from "./components/Greet"
 import {Person} from "./components/Person"
 import {PersonList} from "./components/PersonList"
+import {Status} from "./components/Status"
 import './App.css';
 
 function App() {
@@ -30,6 +31,7 @@ function App() {
       <Greet name="Shubham" messageCount={10} isLoggedIn={true} />
       <Person name={personName} />
       <PersonList names={nameList} />
+      <Status status="success" />
     </div>
   );
 }
