0a155bc9c3fc3afcc6ae109d0dd3f60008383186
Shubham Rajput <rajput.shubh1993@gmail.com>
2023-03-27 16:28:10 +0530

    Advanced Props
    set default value for prop

--- react_typescript/src/components/Greet.tsx
+++ react_typescript/src/components/Greet.tsx
@@ -1,14 +1,16 @@
 type GreetProps = {
   name: string
-  messageCount: number
+  messageCount?: number
   isLoggedIn: boolean
 }
 
 export const Greet = (props: GreetProps) => {
+  const { messageCount = 2} = props
+  
   return(
     <div>
       {
-        props.isLoggedIn ? <h3>Welcome {props.name}! You have {props.messageCount} unread msgs.</h3> : <h3>Welcome Guest</h3>
+        props.isLoggedIn ? <h3>Welcome {props.name}! You have {messageCount} unread msgs.</h3> : <h3>Welcome Guest</h3>
       }
     </div>
   )
