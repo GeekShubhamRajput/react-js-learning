a88afbd6e74887e637dff295f280e2329016d9eb
Shubham Rajput <rajput.shubh1993@gmail.com>
2023-04-18 15:21:45 +0530

    Restricting Props
    Restricting props to provide multiple value

--- react_typescript/src/App.tsx
+++ react_typescript/src/App.tsx
@@ -1,19 +1,10 @@
-import {List} from "./components/generics/List"
+import {RandomNumber} from "./components/restriction/RandomNumber"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
-      <List items={[
-          {
-            id: 1, first_name: 'John', last_name: 'Doe'
-          },
-          {
-            id: 2, first_name: 'Micle', last_name: 'Clark'
-          },
-        ]}
-        onClick={(item)=> console.log(item)} />
+      <RandomNumber value={10} isPositive />
     </div>
   );
 }
