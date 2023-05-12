1d2228c037b02e7ab4b71d1c8ae1d53654ce3da4
Shubham Rajput <rajput.shubh1993@gmail.com>
2023-05-12 12:28:40 +0530

    Redux Thunk Middleware
    Added redux-thunk as middleware

--- react_redux/asyncAction.js
+++ react_redux/asyncAction.js
@@ -1,5 +1,8 @@
 const redux = require('redux')
 const createStore = redux.createStore
+const applyMiddleware = redux.applyMiddleware
+const thunkMiddleware = require('redux-thunk').default
+const axios = require('axios')
 
 const initialState = {
   loading: false,
@@ -9,24 +12,24 @@ const initialState = {
 
 const FETCH_USER_REQUEST = 'FETCH_USER_REQUEST'
 const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS'
-const FETCH_USER_ERROR = 'FETCH_USER_ERROR'
+const FETCH_USER_FAILURE = 'FETCH_USER_FAILURE'
 
 const fetchUsersRequest = () => {
   return {
-    loading: FETCH_USER_REQUEST
+    type: FETCH_USER_REQUEST
   }
 }
 
 const fetchUsersSuccess = (users) => {
   return {
-    loading: FETCH_USER_SUCCESS,
+    type: FETCH_USER_SUCCESS,
     payload: users
   }
 }
 
 const fetchUsersFailure = (errors) => {
   return {
-    loading: FETCH_USER_ERROR,
+    type: FETCH_USER_FAILURE,
     payload: errors
   }
 }
@@ -44,7 +47,7 @@ const reducer = (state=initialState, action) => {
         users: action.payload,
         error: ''
       }
-    case FETCH_USER_ERROR:
+    case FETCH_USER_FAILURE:
       return{
         loading: false,
         users: [],
@@ -53,4 +56,21 @@ const reducer = (state=initialState, action) => {
   }
 }
 
-const store = createStore(reducer)
+const fetchUsers = () => {
+  return function(dispatch){
+    dispatch(fetchUsersRequest())
+
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response=>{
+        const users = response.data.map(user => user.id)
+        dispatch(fetchUsersSuccess(users))
+      })
+      .catch(error => {
+        dispatch(fetchUsersFailure(error.message))
+      })
+  }
+}
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware))
+store.subscribe(()=>console.log(store.getState()))
+store.dispatch(fetchUsers())
