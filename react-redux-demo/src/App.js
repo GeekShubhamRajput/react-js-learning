3b5db973eecd572f63511514c88571001e79847d
Shubham Rajput <rajput.shubh1993@gmail.com>
2023-05-18 16:36:41 +0530

    useSelector Hook
    Create new CakeContainer component using useSelector Hook

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import {Provider} from 'react-redux'
 import store from './redux/store'
 import CakeContainer from './components/CakeContainer'
+import HookCakeContainer from './components/HookCakeContainer'
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HookCakeContainer />
         <CakeContainer />
       </div>
     </Provider>
---
+++ react-redux-demo/src/components/HookCakeContainer.js
@@ -0,0 +1,13 @@
+import {useSelector} from 'react-redux'
+
+function HookCakeContainer(){
+  const numOfCakes = useSelector(state => state.numOfCakes)
+  return(
+    <div>
+      <h2>Number of Cakes - {numOfCakes}</h2>
+      <button>Buy Cake</button>
+    </div>
+  )
+}
+
+export default HookCakeContainer
